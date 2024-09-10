b0083e01c7f737716268937464f49eacda84dcc0
DeepakSan <sjanakisuresh@gmail.com>
2024-09-10 23:38:47 +0530

    Add Other Nav bar links

--- src/blog_web/__init__.py
+++ src/blog_web/__init__.py
@@ -3,13 +3,17 @@ from flask import render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello():
-#     return "Hello, World!"
-
-@app.route('/index','/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
