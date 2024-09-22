46eecfe0376328ec6ba309d570ee3d9004cc15c3
DeepakSan <sjanakisuresh@gmail.com>
2024-09-22 23:40:37 +0530

    Add Auth0 support for flask

--- src/blog_web/__init__.py
+++ src/blog_web/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 from .extensions import db  
 from flask_migrate import Migrate
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import redirect, render_template, session, url_for
 import os
+import json
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('blog_web.config.Config')
-
+    app.secret_key = app.config.get('API_KEY')
     db.init_app(app)  
 
     with app.app_context():
@@ -15,21 +21,69 @@ def create_app():
     migrate = Migrate(app, db)
 
     from .models import Count  
+    oauth = OAuth(app)
+
+    oauth.register(
+        "auth0",
+        client_id=app.config.get("AUTH0_CLIENT_ID"),
+        client_secret=app.config.get("AUTH0_CLIENT_SECRET"),
+        client_kwargs={
+            "scope": "openid profile email",
+        },
+        server_metadata_url=f'https://{app.config.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+    )
 
 
-    @app.route('/index')
+    @app.route("/login")
+    def login():
+        return oauth.auth0.authorize_redirect(
+            redirect_uri=url_for("callback", _external=True)
+        )
+
+    @app.route("/callback", methods=["GET", "POST"])
+    def callback():
+        token = oauth.auth0.authorize_access_token()
+        session["user"] = token
+        return redirect("/")
+
+    @app.route("/logout")
+    def logout():
+        session.clear()
+        return redirect(
+            "https://" + app.config.get("AUTH0_DOMAIN")
+            + "/v2/logout?"
+            + urlencode(
+                {
+                    "returnTo": url_for("index", _external=True),
+                    "client_id": app.config.get("AUTH0_CLIENT_ID"),
+                },
+                quote_via=quote_plus,
+            )
+        )
+    # @app.route("/")
+    # def home():
+    #     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+
+    @app.route('/home')
     def index():
-        new_count = Count(count=0)
-        db.session.add(new_count)
-        db.session.commit()
-        return render_template('index.html')
+        # new_count = Count(count=0)
+        # db.session.add(new_count)
+        # db.session.commit()
+        return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
     @app.route('/contact')
     def contact():
-        return render_template('contact.html')
+        return render_template('contact.html',session=session.get('user'))
 
-    @app.route('/home')
-    def home():
-        return render_template('home.html')
+    # @app.route('/blogs')
+    # def contact():
+    #     return render_template('contact.html',session=session.get('user'))
+    
+
+
+    # @app.route('/home')
+    # def home():
+    #     return render_template('home.html',session=session.get('user'))
 
     return app
--- src/blog_web/config.py
+++ src/blog_web/config.py
@@ -6,5 +6,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     API_KEY = os.environ.get('API_KEY')
+    AUTH0_CLIENT_ID = os.environ.get('AUTH0_CLIENT_ID')
+    AUTH0_CLIENT_SECRET = os.environ.get('AUTH0_CLIENT_SECRET')
+    AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
